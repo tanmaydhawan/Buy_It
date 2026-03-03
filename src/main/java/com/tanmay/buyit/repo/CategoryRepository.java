74c77087a13e9f9dfef62083a0fd717961df6415
tanmaydhawan <tanmaydhawan11@gmail.com>
2026-03-03 17:02:17 +0530

    Added Product and Category Repos.

---
+++ src/main/java/com/tanmay/buyit/repo/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.tanmay.buyit.repo;
+
+import com.tanmay.buyit.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByName (String name);
+}
