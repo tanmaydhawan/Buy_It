0d04728274a9f2845e9c2488f0037e82583839be
tanmaydhawan <tanmaydhawan11@gmail.com>
2026-03-04 17:08:16 +0530

    Product and Category DTOs

---
+++ src/main/java/com/tanmay/buyit/dto/CategoryRequest.java
@@ -0,0 +1,14 @@
+package com.tanmay.buyit.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CategoryRequest {
+    private String name;
+}
---
+++ src/main/java/com/tanmay/buyit/dto/ProductResponse.java
@@ -0,0 +1,22 @@
+package com.tanmay.buyit.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ProductResponse {
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Integer stock;
+
+    private CategoryResponse category;
+}
