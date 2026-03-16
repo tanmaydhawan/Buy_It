acfcfdaf8fefa05735d6747dc02babafc3441f22
tanmaydhawan <tanmaydhawan11@gmail.com>
2026-03-16 23:56:18 +0530

    Added Cart Entity

---
+++ src/main/java/com/tanmay/buyit/entity/Cart.java
@@ -0,0 +1,31 @@
+package com.tanmay.buyit.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cartId;
+
+    @OneToOne
+    private User user;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL)
+    private List<CartItem> cartItemList = new ArrayList<>();
+
+    private BigDecimal totalPrice;
+}
