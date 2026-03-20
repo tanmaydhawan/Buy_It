5e5644eb9db30cceac39d93859a167c31690f620
tanmaydhawan <tanmaydhawan11@gmail.com>
2026-03-20 23:14:39 +0530

    Added Cart Controller

---
+++ src/main/java/com/tanmay/buyit/controller/CartController.java
@@ -0,0 +1,23 @@
+package com.tanmay.buyit.controller;
+
+import com.tanmay.buyit.dto.CartRequest;
+import com.tanmay.buyit.dto.CartResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cart")
+@AllArgsConstructor
+public class CartController {
+
+    private final CartService cartService;
+
+    @PostMapping("/items")
+    public ResponseEntity<CartResponse> addToCart(@RequestBody CartRequest cartRequest){
+        return ResponseEntity.ok(cartService.addToCart(cartRequest));
+    }
+}
