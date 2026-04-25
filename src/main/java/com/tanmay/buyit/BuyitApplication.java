c3ddc16b38bb4f72b67b6281c05cbf21fdc86653
tanmaydhawan <tanmaydhawan11@gmail.com>
2026-04-25 23:29:44 +0530

    Enabled Caching in Buyit application

--- src/main/java/com/tanmay/buyit/BuyitApplication.java
+++ src/main/java/com/tanmay/buyit/BuyitApplication.java
@@ -2,8 +2,10 @@ package com.tanmay.buyit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class BuyitApplication {
 
 	public static void main(String[] args) {
