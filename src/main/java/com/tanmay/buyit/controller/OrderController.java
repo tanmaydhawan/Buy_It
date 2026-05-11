a040c60519bab2b8ae048369e407ce89509ce54b
tanmaydhawan <tanmaydhawan11@gmail.com>
2026-05-11 23:57:09 +0530

    Added Tag to Order API

--- src/main/java/com/tanmay/buyit/controller/OrderController.java
+++ src/main/java/com/tanmay/buyit/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.tanmay.buyit.controller;
 
 import com.tanmay.buyit.dto.OrderResponse;
 import com.tanmay.buyit.service.OrderService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/order")
 @AllArgsConstructor
+@Tag(name = "Order APIs", description = "Operations related to placing Order")
 public class OrderController {
 
     private final OrderService orderService;
