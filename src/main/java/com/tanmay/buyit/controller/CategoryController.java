0ecf4af88e2899fa208a2431a62d916cd268f76a
tanmaydhawan <tanmaydhawan11@gmail.com>
2026-05-12 21:08:44 +0530

    Adding Swagger to Category Api

--- src/main/java/com/tanmay/buyit/controller/CategoryController.java
+++ src/main/java/com/tanmay/buyit/controller/CategoryController.java
@@ -3,6 +3,8 @@ package com.tanmay.buyit.controller;
 import com.tanmay.buyit.dto.CategoryRequest;
 import com.tanmay.buyit.dto.CategoryResponse;
 import com.tanmay.buyit.service.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,24 +17,28 @@ import java.util.List;
 @RestController
 @RequestMapping("/category")
 @AllArgsConstructor
+@Tag(name = "Category APIs", description = "Operations related to Categories")
 public class CategoryController {
 
     private final CategoryService categoryService;
 
     @GetMapping
     @PreAuthorize("hasAuthority('BUYIT_ADMIN')")
+    @Operation(summary = "Loading all Categories")
     public ResponseEntity<List<CategoryResponse>> fetchAllCategories(){
         return new ResponseEntity<>(categoryService.getAllCategories(), HttpStatus.OK);
     }
 
     @PostMapping
     @PreAuthorize("hasAuthority('BUYIT_ADMIN')")
+    @Operation(summary = "Creating a new Category")
     public ResponseEntity<CategoryResponse> createCategory(@Valid @RequestBody CategoryRequest categoryRequest){
         return new ResponseEntity<>(categoryService.createCategory(categoryRequest), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
     @PreAuthorize("hasAuthority('BUYIT_ADMIN')")
+    @Operation(summary = "Delete an existing Category")
     public ResponseEntity<?> deleteCategory (@PathVariable Long id){
         categoryService.deleteCategoryById(id);
         return ResponseEntity.ok("Category Deleted Successfully");
