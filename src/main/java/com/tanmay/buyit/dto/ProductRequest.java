b85b9812c225c2151abfaec7d7ee00cef07b1f53
tanmaydhawan <tanmaydhawan11@gmail.com>
2026-05-15 23:27:33 +0530

    Added @Schema to Product Request DTO

--- src/main/java/com/tanmay/buyit/dto/ProductRequest.java
+++ src/main/java/com/tanmay/buyit/dto/ProductRequest.java
@@ -1,5 +1,6 @@
 package com.tanmay.buyit.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,20 +17,25 @@ public class ProductRequest {
 
     @NotBlank
     @Size(min = 3, max = 50)
+    @Schema(description = "Product name", example = "iPhone 18")
     private String name;
 
     @NotBlank
     @Size(min = 3, max = 100)
+    @Schema(description = "Product description", example = "iPhone 18 the brand new same phone")
     private String description;
 
     @NotNull
     @DecimalMin("0.01")
+    @Schema(description = "Product price", example = "99999")
     private BigDecimal price;
 
     @NotNull
     @Min(0)
+    @Schema(description = "Product stock", example = "999")
     private Integer stock;
 
     @NotNull
+    @Schema(description = "Product category id", example = "18")
     private Long categoryId;
 }
