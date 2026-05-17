e167759f5bfee7f1d28cc7fc10af05819aeaa497
tanmaydhawan <tanmaydhawan11@gmail.com>
2026-05-17 23:05:06 +0530

    Added @Schema to Login Request DTO

--- src/main/java/com/tanmay/buyit/dto/LoginRequest.java
+++ src/main/java/com/tanmay/buyit/dto/LoginRequest.java
@@ -1,5 +1,6 @@
 package com.tanmay.buyit.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -8,10 +9,12 @@ public class LoginRequest {
 
     @NotBlank
     @Email
+    @Schema(description = "User Email", example = "tanmaydhawan18@gmail.com")
     private String email;
 
     @NotBlank
     @Size(min = 8, message = "Password must be at least 8 characters")
+    @Schema(description = "Account Password", example = "SamplePassword69")
     private String password;
 
     public String getEmail() {
