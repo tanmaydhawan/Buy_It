ca9330c6443147021fb8153440877810375bb5bc
tanmaydhawan <tanmaydhawan11@gmail.com>
2026-05-19 23:33:15 +0530

    Added Schema defination to Category DTO

--- src/main/java/com/tanmay/buyit/dto/CategoryRequest.java
+++ src/main/java/com/tanmay/buyit/dto/CategoryRequest.java
@@ -1,5 +1,6 @@
 package com.tanmay.buyit.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -15,5 +16,6 @@ public class CategoryRequest {
 
     @NotBlank
     @Size(min = 3 , max = 30)
+    @Schema(description = "Category name", example = "Electronics")
     private String name;
 }
