9f3930cf841e1a8ab7bf2d54771bb3c6bc6574e3
tanmaydhawan <tanmaydhawan11@gmail.com>
2026-05-20 22:47:52 +0530

    Added schema defination to Cart Request

--- src/main/java/com/tanmay/buyit/dto/CartRequest.java
+++ src/main/java/com/tanmay/buyit/dto/CartRequest.java
@@ -1,5 +1,6 @@
 package com.tanmay.buyit.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
@@ -15,10 +16,12 @@ import lombok.NoArgsConstructor;
 public class CartRequest {
 
     @NotNull
+    @Schema(description = "Product Id", example = "99")
     private Long productId;
 
     @NotNull
     @Positive
     @Max(99)
+    @Schema(description = "Product Quantity", example = "999")
     private Integer quantity;
 }
